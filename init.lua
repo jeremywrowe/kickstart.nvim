00eaa87633d43a4b8d2d1e9aaae4fa371e87a679
Jeremy W. Rowe <jeremy.rowe@shopify.com>
2024-07-01 13:15:44 -0400

    rust dap :tada:

--- init.lua
+++ init.lua
@@ -297,6 +297,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format lua code
+        'codelldb', -- Used to debug rust/cpp
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -414,33 +415,41 @@ require('lazy').setup({
   },
 
   {
-    'rcarriga/nvim-dap-ui',
-    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio' },
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    lazy = false, -- This plugin is already lazy
     config = function()
-      local dap = require 'dap'
+      local dap, dapui = require 'dap', require 'dapui'
 
-      dap.adapters.codelldb = {
-        type = 'server',
-        port = '${port}',
-        executable = {
-          command = '/usr/bin/codelldb',
-          args = { '--port', '${port}' },
-        },
-      }
+      vim.keymap.set('n', '<Leader>dt', dap.toggle_breakpoint, { desc = 'Debug Toggle Breakpoint' })
+      vim.keymap.set('n', '<Leader>dc', dap.continue, { desc = 'Debug Continue' })
+      vim.keymap.set('n', '<Leader>di', dap.step_into, { desc = 'Debug Step Into' })
+      vim.keymap.set('n', '<Leader>dov', dap.step_over, { desc = 'Debug Step Over' })
+      vim.keymap.set('n', '<Leader>dot', dap.step_out, { desc = 'Debug Step Out' })
+      vim.keymap.set('n', '<Leader>dx', dap.terminate, { desc = 'Debug Exit' })
+      vim.keymap.set('n', '<Leader>dr', dap.restart_frame, { desc = 'Debug Restart Frame' })
 
-      dap.configurations.rust = {
-        {
-          name = 'Rust debug',
-          type = 'codelldb',
-          request = 'launch',
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
-          end,
-          cwd = '${workspaceFolder}',
-          stopOnEntry = true,
-        },
-      }
+      vim.keymap.set('n', '<Leader>dr', ':RustLsp debuggables<CR>', { desc = 'Rust LSP Debuggables' })
+
+      dapui.setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
     end,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = { 'mfussenegger/nvim-dap', 'nvim-neotest/nvim-nio', 'mrcjkb/rustaceanvim' },
   },
 
   { -- Highlight, edit, and navigate code
